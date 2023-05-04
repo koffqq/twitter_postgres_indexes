7818e0ab823ff385067c7a57c621559c3da6c4ca
Yaqin Zhang <yzhang23@cmc.edu>
2023-05-03 20:14:02 -0700

    add sql-indexes

---
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX tag_id_tweets_index ON tweet_tags(tag,id_tweets);
+CREATE INDEX id_tweets_tag_index ON tweet_tags (id_tweets, tag);
+CREATE INDEX id_tweets_lang_index ON tweets(id_tweets, lang);
+CREATE INDEX lang_index ON tweets(lang);
+CREATE INDEX gin_index ON tweets USING gin(to_tsvector('english', text));
